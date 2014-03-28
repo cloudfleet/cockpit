6aab42945dc949ca860ff0a2a283cd8b78d91ae4
Dražen Lučanin <kermit666@gmail.com>
2014-03-28 15:23:40 +0200

    dockerfile for properly setting up a pagekite backend

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-usage="start.sh <domain> <secret>"
+usage="start.sh <domain> <secret> [frontend]\n\
+e.g. start.sh test.blimpyard.cloudfleet.io supersecret blimpyard.cloudfleet.io:1234"
 
-if [ $# -ne 2 ]; then
+if [ $# -lt 2 ]; then
     echo "No parameter passed, not starting pagekite. Usage:";
     echo $usage
 else
     domain=$1
     secret=$2
     echo "Connecting pagekite.";
-    backend_config="domain=https:${domain}:${secret}"
-    echo $backend_config > /etc/pagekite.d/20_backend.rc
+
+    # account
+    account_rc="kitename = ${domain}\nkitesecret = ${secret}"
+    echo -e $account_rc > /etc/pagekite.d/10_account.rc
+
+    # frontend
+    frontend=${3:-blimpyard.cloudfleet.io:60666}
+    frontend_rc="frontend = ${frontend}"
+    echo $frontend_rc > /etc/pagekite.d/20_frontends.rc
+
+    # httpd
+    httpd_rc="service_on = http:@kitename : localhost:3000 : @kitesecret"
+    echo $httpd_rc > /etc/pagekite.d/80_httpd.rc
 fi
 
 cd $HOME/cockpit
 
 # start pagekite backend daemon
-sudo service pagekite start
+#sudo service pagekite start
+sudo invoke-rc.d pagekite restart
 
 # the app
 node cockpit.js
